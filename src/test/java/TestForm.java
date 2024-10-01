55c7565763ac4c9ed077787a6e43dd2f759d054f
Tapochek951 <noobkillbuster@gmail.com>
2024-10-01 21:04:23 +0500

    +++

--- src/test/java/TestForm.java
+++ src/test/java/TestForm.java
@@ -1,23 +1,39 @@
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestForm {
-    @Test
-    void successfulSearchTest() {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "2160x1440";
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = true;
-        open("https://demoqa.com/automation-practice-form");
-        $("#firstName").setValue("Huesos");
-        $("#lastName").setValue("Pidor");
+    }
+    @Test
+    void successfulSearchTest() {
+
+        open("/automation-practice-form");
+        $("#firstName").setValue("Tapok");
+        $("#lastName").setValue("Worker");
+        $("#gender-radio-1").parent().click();
         $("#userNumber").setValue("1234567890");
-        $("#userEmail").setValue("huesos@gmail.com");
+        $("#userEmail").setValue("tapochek@gmail.com");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("January");
+        $(".react-datepicker__year-select").selectOption("1997");
+        $(".react-datepicker__day--021").click();
         $("#subjectsInput").setValue("Physics").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
         $("#currentAddress").setValue("basileevka");
         $("#uploadPicture").uploadFromClasspath("picure.jpg");
+
         $("#submit").click();
 
     }
