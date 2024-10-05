3c25e1ff4a6164199e9f6dc4dc67bcc21caba5e4
Tapochek951 <noobkillbuster@gmail.com>
2024-10-05 14:55:12 +0500

    наконец допер

--- src/test/java/IshemReposit.java
+++ src/test/java/IshemReposit.java
@@ -1,19 +1,31 @@
 package github;
 
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class IshemReposit {
 
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "2160x1440";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+}
     @Test
     void shouldFindSelenideRepositoryAtTheTop() {
 
 
         open("https://github.com");
+        $("[placeholder='Search or jump to...']").click();
+        $("#query-builder-test").setValue("selenide").pressEnter();
 
     }
 }
